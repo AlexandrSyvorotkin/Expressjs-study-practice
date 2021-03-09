967f9af80c6a7b9db3b22347e2254f2586e6315b
Alex <Aleks031994@mail.ru>
2021-03-09 22:22:11 +0300

    mongodb and mogoose connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const cardRoutes = require('./routes/card')
 const homeRoutes = require('./routes/home')
@@ -26,9 +27,42 @@ app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 
 
+async function start() {
+    try {
+        console.log('MongoDB conncted');
+        const url = `mongodb+srv://Alex:Sanek9955@nodejscoursecluster.7c8it.mongodb.net/easycode?retryWrites=true&w=majority`
+        await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology:true})
+        app.listen(PORT, ()=> {
+            console.log('MongoDB conncted');
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch(e) {
+        console.log(e);
+    }
+    
+}
+
+start()
+
+
+
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
-})
+
+
+
+
+// mongoose
+//     .connect('mongodb+srv://Alex:Sanek9955@nodejscoursecluster.7c8it.mongodb.net/easycode?retryWrites=true&w=majority',
+//     {
+//         useNewUrlParser: true
+//     },
+// )
+// .then(() => {console.log('MongoDb connected');})
+// .catch(err => console.log(err));
+
+// TODO: Разобраться с паролями и урлами
+// const url = `mongodb+srv://Alex:<password>@nodejscoursecluster.7c8it.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+// const password ='okso0PKmWUpURgdo'
 
